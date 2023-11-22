612509c45c01e540eef62dc6dbac922271bf5f8e
vgtoui <toni.uimonen@tuni.fi>
2023-11-22 16:53:23 +0200

    Create tests for IndexController and Person

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -0,0 +1,33 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+
+    @Test
+    void groupedAssertions() {
+        // given
+        Person person = new Person(101l, "Ken", "Climo");
+
+        // then
+        assertAll("Test Props Set",
+                () -> assertEquals( "Ken", person.getFirstName()),
+                () -> assertEquals("Climo", person.getLastName()));
+    }
+
+    // Differs from the previous test only in terms of the message.
+    @Test
+    void groupedAssertionsMsgs() {
+        // given
+        Person person = new Person(101l, "Ken", "Climo");
+
+        // then
+        assertAll("Test Props Set",
+                () -> assertEquals("Ken", person.getFirstName(), "First Name Failed"),
+                () -> assertEquals( "Climo", person.getLastName(), "Last Name Failed"));
+    }
+
+}
